9b7f65c8c8c6f51118170b3cdafcf0c49b5b1dfe
049341 <049341@UNI-LAB2-44>
2025-02-18 20:09:11 -0300

    Adicionar arquivos de projeto.

---
+++ Media/Program.cs
@@ -0,0 +1,18 @@
+﻿double a, b, c;
+double media;
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+c = int.Parse(Console.ReadLine());
+
+media = (a + b + c) / 3;
+
+
+if (media > 3.4)
+    {
+    Console.WriteLine("Média maior que a ponderada.");
+}
+ else
+{
+    Console.WriteLine("Média:");
+}
+Console.WriteLine(media);
